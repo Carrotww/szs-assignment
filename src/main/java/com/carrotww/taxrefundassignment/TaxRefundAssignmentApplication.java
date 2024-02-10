d0ba90b077a12bb04d004322b389c34e287b30f2
Carrotww <rjgjfl90@naver.com>
2024-02-10 11:36:40 +0900

    init

---
+++ src/main/java/com/carrotww/taxrefundassignment/TaxRefundAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.carrotww.taxrefundassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxRefundAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxRefundAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrotww/taxrefundassignment/TaxRefundAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrotww.taxrefundassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxRefundAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
